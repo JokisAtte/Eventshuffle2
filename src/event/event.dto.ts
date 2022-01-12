fa53c5884ff0405b0e273c92a1d94353df622c45
Atte Jokinen <atte.jokinen@tuni.fi>
2022-01-12 23:43:25 +0200

    improved date validation

--- src/event/event.dto.ts
+++ src/event/event.dto.ts
@@ -4,18 +4,21 @@ import {
   ArrayNotEmpty,
   IsNotEmpty,
   IsString,
+  Matches,
   MaxLength,
 } from 'class-validator';
 
 export class CreateEventDto {
   @IsString()
   @IsNotEmpty()
-  @MaxLength(60)
+  @MaxLength(40)
   @ApiProperty()
   name: string;
 
   @ArrayNotEmpty()
-  @ArrayMaxSize(30)
+  @ArrayMaxSize(10)
   @ApiProperty()
+  @IsString({ each: true })
+  @Matches(/[0-9]{4}-[0-9]{2}-[0-9]{2}/, { each: true })
   dates: string[];
 }
